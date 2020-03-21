2f324c432695d2c5080a1091576a0a1ae6aac996
Samuel Meuli <me@samuelmeuli.com>
2020-03-21 21:09:17 +0100

    Add support for more UTIs and file extensions

--- QLPlugin/Renderers/FileTypeRenderers/CodeRenderer.swift
+++ QLPlugin/Renderers/FileTypeRenderers/CodeRenderer.swift
@@ -1,8 +1,38 @@
 import Foundation
 import os.log
 
+let dotfileLexers = [
+	".dockerignore": "bash",
+	".editorconfig": "ini",
+	".gitattributes": "bash",
+	".gitignore": "bash",
+	".npmignore": "bash",
+]
+
+let fileExtensionLexers = [
+	"alfredappearance": "json", // Alfred theme
+	"cls": "tex", // LaTeX classes file
+	"entitlements": "xml",
+	"iml": "xml",
+	"plist": "xml",
+	"resolved": "json", // Swift Package Manager lockfile (Package.resolved)
+	"sty": "tex", // LaTeX styles file
+]
+
 class CodeRenderer: Renderer {
 	private let chromaBinUrl = Bundle.main.url(forAuxiliaryExecutable: "chroma-v0.7.0")
+
+	/// Returns the name of the Chroma lexer to use for the file. This is determined based on the
+	/// file name/extension
+	private func getLexer() -> String {
+		if fileExtension.isEmpty {
+			// Dotfile
+			return dotfileLexers[fileUrl.lastPathComponent, default: "autodetect"]
+		} else {
+			// File with extension
+			return fileExtensionLexers[fileExtension, default: "autodetect"]
+		}
+	}
 
 	override func getStylesheets() -> [Stylesheet] {
 		var stylesheets = super.getStylesheets()
@@ -22,7 +52,7 @@ class CodeRenderer: Renderer {
 
 		let (status, stdout, stderr) = Shell.run(
 			url: binaryUrlResolved,
-			arguments: [fileUrl.path, "--html", "--html-only"]
+			arguments: [fileUrl.path, "--html", "--html-only", "--lexer", getLexer()]
 		)
 
 		guard status == 0 else {
