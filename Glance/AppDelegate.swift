cc7084a7a462ee84702714f231a7a72c631127be
Samuel Meuli <me@samuelmeuli.com>
2020-04-04 15:12:01 +0200

    Add main app

--- Glance/AppDelegate.swift
+++ Glance/AppDelegate.swift
@@ -1,33 +1,14 @@
 import Cocoa
 
-let APP_NAME = Bundle.main.infoDictionary![kCFBundleNameKey as String] as! String
-
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-	var window: NSWindow!
-
-	override init() {
-		super.init()
-	}
-
 	func applicationDidFinishLaunching(_: Notification) {
-		createWindow()
+		// Insert code here to initialize your application
 	}
 
-	func applicationWillTerminate(_: Notification) {}
-
-	func createWindow() {
-		window = NSWindow(
-			contentRect: .init(
-				origin: .zero,
-				size: .init(width: NSScreen.main!.frame.midX, height: NSScreen.main!.frame.midY)
-			),
-			styleMask: [.closable, .miniaturizable, .titled],
-			backing: .buffered,
-			defer: false
-		)
-		window.center()
-		window.title = APP_NAME
-		window.makeKeyAndOrderFront(nil)
+	func applicationWillTerminate(_: Notification) {
+		// Insert code here to tear down your application
 	}
+
+	func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool { true }
 }
