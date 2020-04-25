909d01f7d733c040fa0f4da46c7cad10a330cfa5
Samuel Meuli <me@samuelmeuli.com>
2020-04-25 17:23:29 +0200

    Add value transformers for outline view
    - Add file/directory icon next to file name
    - Show "--" instead of "0 bytes" in file size column (for consistency with Finder)

--- QLPlugin/Views/PreviewVCs/OutlinePreviewVC.swift
+++ QLPlugin/Views/PreviewVCs/OutlinePreviewVC.swift
@@ -21,6 +21,10 @@ class OutlinePreviewVC: NSViewController, PreviewVC {
 		self.root = root
 		self.labelText = labelText
 		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+		// Register required value transformers
+		ValueTransformer.setValueTransformer(IconTransformer(), forName: .iconTransformerName)
+		ValueTransformer.setValueTransformer(SizeTransformer(), forName: .sizeTransformerName)
 	}
 
 	@available(*, unavailable)
@@ -49,4 +53,53 @@ class OutlinePreviewVC: NSViewController, PreviewVC {
 			outlineView.expandItem(node)
 		}
 	}
+}
+
+/// `ValueTransformer` which returns the correct icon depending on whether the current row
+/// represents a file or directory.
+class IconTransformer: ValueTransformer {
+	let directoryIcon = NSImage(
+		contentsOfFile: "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericFolderIcon.icns"
+	)
+	let fileIcon = NSImage(
+		contentsOfFile: "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericDocumentIcon.icns"
+	)
+
+	override class func transformedValueClass() -> AnyClass { NSImage.self }
+
+	override class func allowsReverseTransformation() -> Bool { false }
+
+	override func transformedValue(_ value: Any?) -> Any? {
+		guard let isDirectoryNumber = value as? NSNumber else {
+			return nil
+		}
+		let isDirectory = Bool(truncating: isDirectoryNumber)
+		return isDirectory ? directoryIcon : fileIcon
+	}
+}
+
+/// `ValueTransformer` which formats the provided number of bytes as a human-readable string (e.g.
+/// `12345` -> `"12.345 KB"` or `0` -> `"--"`).
+class SizeTransformer: ValueTransformer {
+	let byteCountFormatter = ByteCountFormatter()
+	let fallbackValue = "--"
+
+	override class func transformedValueClass() -> AnyClass { NSString.self }
+
+	override class func allowsReverseTransformation() -> Bool { false }
+
+	override func transformedValue(_ value: Any?) -> Any? {
+		guard let size = value as? NSNumber else {
+			return nil
+		}
+
+		// Format number of bytes in human-readable way. If the size is 0 bytes, return "--" instead
+		// (same behavior as Finder)
+		return size == 0 ? fallbackValue : (byteCountFormatter.string(for: size) ?? fallbackValue)
+	}
+}
+
+extension NSValueTransformerName {
+	static let iconTransformerName = NSValueTransformerName(rawValue: "IconTransformer")
+	static let sizeTransformerName = NSValueTransformerName(rawValue: "SizeTransformer")
 }
